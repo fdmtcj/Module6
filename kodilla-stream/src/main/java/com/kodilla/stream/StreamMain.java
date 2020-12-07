a91438935a700e09d626fd07460125b8344287de
Jakub Rej <jrej@edu.cdv.pl>
2020-12-07 09:59:38 +0100

    Module 7

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -6,18 +6,22 @@ import com.kodilla.stream.beautifier.PoemBeautifier;
 
 public class StreamMain {
     public static void main(String[] args) {
-    //Zadanie 7.1
+
+        //Zadanie 7.1
+
         PoemBeautifier poemBeautifier = new PoemBeautifier();
+
         poemBeautifier.beautify("PIERWSZY", String::toLowerCase);
         poemBeautifier.beautify("drugi", String::toUpperCase);
-        poemBeautifier.beautify("Trzeci",(text)->"*** "+text +" ***");
-        poemBeautifier.beautify("Czwarty",(text)->text.substring(1,text.length()-1));
-        poemBeautifier.beautify("AAAPiątyAAA",(text)->text.replace("A",""));
+        poemBeautifier.beautify("Trzeci", (text) -> "*** " + text + " ***");
+        poemBeautifier.beautify("Czwarty", (text) -> text.substring(1, text.length() - 1));
+        poemBeautifier.beautify("AAAPiątyAAA", (text) -> text.replace("A", ""));
 
         System.out.println();
     }
-
 }
+
+
 
 
 
