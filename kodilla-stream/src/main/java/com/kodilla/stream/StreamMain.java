d4af14bc190e5baef3454ba6a6ff9bf6a61eb386
Jakub Rej <jrej@edu.cdv.pl>
2020-12-13 22:22:08 +0100

    Module 7

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,48 +1,25 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 
 public class StreamMain {
     public static void main(String[] args) {
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()  // [1]
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));                    // [2]
 
-        //Zadanie 7.1
-
-        //    PoemBeautifier poemBeautifier = new PoemBeautifier();
-
-        //    poemBeautifier.beautify("PIERWSZY", String::toLowerCase);
-        //    poemBeautifier.beautify("drugi", String::toUpperCase);
-        //    poemBeautifier.beautify("Trzeci", (text) -> "*** " + text + " ***");
-        //    poemBeautifier.beautify("Czwarty", (text) -> text.substring(1, text.length() - 1));
-        //    poemBeautifier.beautify("AAAPiątyAAA", (text) -> text.replace("A", ""));
-
-        //    System.out.println();
-        //    System.out.println("Using Stream to generate even numbers from 1 to 20");
-        //    NumbersGenerator.generateEven(20);
-
-        //Zadanie 7.3
-
-       /* Forum forum = new Forum();
-
-        Map<Integer, ForumUser> theResultMapOfUsers = forum.getList().stream()
-                .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter(forumUser -> forumUser.getBirthDate().isBefore(LocalDate.now().minusYears(20)))
-                .filter(forumUser -> forumUser.getPostsAmount() > 1)
-                .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
-
-        System.out.println("# elements: " + theResultMapOfUsers.size());
-        theResultMapOfUsers.entrySet().stream()
-                .map(entry -> entry.getKey() + ": " + entry.getValue())
-                .forEach(System.out::println);
-
-        System.out.println();
-        */
-
+        System.out.println(theResultStringOfBooks);
     }
 }
 
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    public void testGetAverage() {
+        //Given
+        int[] numbers = {12, 89, 91, 34, 72, 4, 41};
+        int[] numbers2 = new int[0];
+
+        //When
+        double average = ArrayOperations.getAverage(numbers);
+        double average2 = ArrayOperations.getAverage(numbers2);
+
+        //Then
+        Assert.assertEquals(49.0, average, 0.01);
+        Assert.assertEquals(0, average2, 0.01);
+
+    }
+}
