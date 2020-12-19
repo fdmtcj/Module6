f78596b95427f6abe7e8e00fcf771aea4a0160b4
Jakub Rej <jrej@edu.cdv.pl>
2020-12-19 15:33:13 +0100

    Module 8

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
@@ -0,0 +1,19 @@
+package com.kodilla.exception.test;
+
+public class FlightRunner {
+
+    public static void main(String[] args) {
+
+        Flight budapeszt = new Flight("Warszawa", "Budapeszt");
+        Flight kijów = new Flight("Warszawa", "Kijów");
+        FlightSeeker flightSeeker = new FlightSeeker();
+
+        try {
+            flightSeeker.findFlight(kijów);
+
+        } catch (RouteNotFoundException e) {
+            System.out.println("Can't find that flight, try another.");
+        }
+    }
+}
+
