9314a33484c8895d5cff7748bd46533b4b865638
Jakub Rej <jrej@edu.cdv.pl>
2020-12-19 11:56:40 +0100

    Module 8

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -9,15 +9,15 @@ public class ArrayOperationsTestSuite {
     public void testGetAverage() {
         //Given
         int[] numbers = {12, 89, 91, 34, 72, 4, 41};
-        int[] numbers2 = new int[0];
+
 
         //When
         double average = ArrayOperations.getAverage(numbers);
-        double average2 = ArrayOperations.getAverage(numbers2);
+
 
         //Then
         Assert.assertEquals(49.0, average, 0.01);
-        Assert.assertEquals(0, average2, 0.01);
+
 
     }
 
