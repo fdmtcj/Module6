d9cc8d07ec692cc84d8fe9916eda2ffc3a2be8ca
Jakub Rej <jrej@edu.cdv.pl>
2021-02-08 21:18:42 +0100

    Module 14

---
+++ src/test/java/com/kodilla/spring/web2/KodillaSpringWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.web2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaSpringWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
