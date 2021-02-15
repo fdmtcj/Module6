dcd2ae3914eb71aa8faeba1622d95aed885690fa
Jakub Rej <jrej@edu.cdv.pl>
2021-02-15 20:44:34 +0100

    Module 14

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,7 +1,7 @@
 package com.kodilla.spring.portfolio;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -19,8 +19,8 @@ public class BoardTestSuite {
         board.getDoneList().addTask("DoneTask2");
 
         //Then
-        Assert.assertEquals("ToDoTask3", board.getToDoList().getTasks().get(board.getToDoList().getTasks().size() - 1));
-        Assert.assertEquals("InProgressTask4", board.getInProgressList().getTasks().get(board.getInProgressList().getTasks().size() - 1));
-        Assert.assertEquals("DoneTask2", board.getDoneList().getTasks().get(board.getDoneList().getTasks().size() - 1));
+        Assert.assertEquals("ToDoTask3", board.getToDoList().getTasks().get(2));
+        Assert.assertEquals("InProgressTask4", board.getInProgressList().getTasks().get(3));
+        Assert.assertEquals("DoneTask2", board.getDoneList().getTasks().get(1));
     }
 }
